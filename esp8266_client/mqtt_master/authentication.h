349cf6111486aa34aa0cb8cd3a7f68ea3e175847
geraldvm <gvmckenzie@outlook.com>
2022-11-11 22:16:09 -0600

    MQTT Arduino Client

---
+++ esp8266_client/mqtt_master/authentication.h
@@ -0,0 +1,24 @@
+
+
+/*  Node-RED and ESP8266
+ * 
+ * This code is property of Mckode Development
+ * For information, please see
+ * 
+ * https://mckode.com
+ * 
+ * For information on hardware components and the wiring needed to 
+ * run this sketch, please see our wiki on https://github.com/geraldvm/agro_mcu
+ * 
+ *  
+ *  Created by Gerald V. Mc kenzie
+ * 
+ */
+
+#define SECRET_SSID  "Mckode"
+#define SECRET_PASS  "password"
+#define BROKER_USERNAME  "agrobyte"
+#define BROKER_PASSWORD  "7890hjuil"
+#define BROKER_IP  "ec2-52-201-176-232.compute-1.amazonaws.com"
+#define CLIENT_NAME  "mcuAgrbyte435"
+#define BROKER_PORT  1883
